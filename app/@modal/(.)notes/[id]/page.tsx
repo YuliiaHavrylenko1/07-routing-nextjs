c9cb97ed2afe1976f2035f5a138bd25406beec2b
YuliiaHavrylenko1 <gavrilenko.j94@gmail.com>
2025-08-24 21:17:57 +0100

    last

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,17 +1,4 @@
-// import NotePreviewClient from './modalPreview.client'; 
-// import { fetchNoteById } from '@/lib/api';
 
-// type Props = {
-//   params: { id: string };
-// };
-
-// const NotePreview = async ({ params }: Props) => {
-//   const note = await fetchNoteById(params.id);
-
-//   return <NotePreviewClient note={note} />;
-// };
-
-// export default NotePreview;
 
 import NotePreviewClient from './modalPreview.client';
 import { fetchNoteById } from '@/lib/api';
