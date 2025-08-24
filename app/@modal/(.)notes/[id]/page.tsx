f65ea05a6e46363468882b736ec5d23f7abcd753
YuliiaHavrylenko1 <gavrilenko.j94@gmail.com>
2025-08-24 21:32:49 +0100

    changed page.tsx

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,15 +1,29 @@
 
 
+// import NotePreviewClient from './modalPreview.client';
+// import { fetchNoteById } from '@/lib/api';
+
+// type Props = {
+//   params: Promise<{ id: string }>; 
+// };
+
+// export default async function NotePreview({ params }: Props) {
+//   const { id } = await params;     
+//   const note = await fetchNoteById(id);
+
+//   return <NotePreviewClient note={note} />;
+// }
+
+
 import NotePreviewClient from './modalPreview.client';
 import { fetchNoteById } from '@/lib/api';
 
 type Props = {
-  params: Promise<{ id: string }>; 
+  params: { id: string }; 
 };
 
 export default async function NotePreview({ params }: Props) {
-  const { id } = await params;     
-  const note = await fetchNoteById(id);
+  const note = await fetchNoteById(params.id);
 
   return <NotePreviewClient note={note} />;
 }
